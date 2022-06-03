8a6925c5514359effb885744713c758e8799e434
mainuddin-sani <main12sani@gmail.com>
2022-06-03 15:22:59 +0600

    email password connect firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import app from "./firebase.init";
+import { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 function App() {
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState('');
+  // google sign in click event handler
+  const googleSignInHandler = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      const user = result.user;
+      setUser(user);
+    });
+  };
+
+  const googleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        setUser("");
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  // email
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const passwordHandler = (e)=>{
+    setPassword(e.target.value);
+  }
+  console.log(email);
+  const onSubmitHandler = (e)=> {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user.uid ? (
+        <button onClick={googleSignOut}>Google sign Out</button>
+      ) : (
+        <button onClick={googleSignInHandler}>Google sign In</button>
+      )}
+
+      <div className="card">
+        {user.photoURL ? (
+          <img src={user.photoURL} alt="" />
+        ) : (
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt=""
+          />
+        )}
+
+        <div>
+          <h4>Name : {user.displayName}</h4>
+          <p>Email : {user.email}</p>
+        </div>
+      </div>
+      <Container>
+        <Row>
+          <Col lg={4} className="mx-auto">
+            <Form onClick={onSubmitHandler}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  onBlur={emailHandler}
+                  placeholder="Enter email"
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" onBlur={passwordHandler} placeholder="Password" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmInX05gY16HCJP2T5wjvn6wO-L_znku8",
+  authDomain: "practice-authentication-ab3a8.firebaseapp.com",
+  projectId: "practice-authentication-ab3a8",
+  storageBucket: "practice-authentication-ab3a8.appspot.com",
+  messagingSenderId: "425062527544",
+  appId: "1:425062527544:web:6700e2823d3249bdb2eccd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
